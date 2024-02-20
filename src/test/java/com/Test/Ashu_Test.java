39dd4681b24566599492d10227688bc815ce5c09
AnuAshu <ashwini.waghole@gmail.com>
2024-02-20 09:13:11 +0530

    Ashu_Test.java file added into project

---
+++ src/test/java/com/Test/Ashu_Test.java
@@ -0,0 +1,12 @@
+package com.Test;
+
+public class Ashu_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("For testing Git Commands");
+
+	}
+
+}
